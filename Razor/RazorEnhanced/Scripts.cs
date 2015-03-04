f6402178e587b1b6b636f143821db00e3daf408b
marco <marco.carlotto@gmail.com>
2015-03-04 09:57:32 +0100

    mod allo Script engine: passaggio da thread a managed task e aggiunta ConcurrentQueue di comandi RunMode

--- Razor/RazorEnhanced/Scripts.cs
+++ Razor/RazorEnhanced/Scripts.cs
@@ -1,41 +1,65 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.IO;
-using System.Text;
 using System.Reflection;
-using Assistant;
-using System.Threading.Tasks;
-using PaxScript.Net;
 using System.Threading;
+using System.Threading.Tasks;
+using Assistant;
+using PaxScript.Net;
+
 
 namespace RazorEnhanced
 {
 	internal class Scripts
 	{
-		private static TimeSpan m_Delay = TimeSpan.FromMilliseconds(25.0);
-
 		internal class ScriptWorker
 		{
+			private ConcurrentQueue<RunMode> m_Actions = new ConcurrentQueue<RunMode>();
+			internal ConcurrentQueue<RunMode> Actions { get { return m_Actions; } }
+
 			private EnhancedScript m_Script;
 			internal EnhancedScript Script { get { return m_Script; } }
 
-			internal ScriptWorker(EnhancedScript script)
+			private TimeSpan m_Delay;
+			internal TimeSpan Delay { get { return m_Delay; } }
+
+			private bool m_AutoMode;
+			internal bool AutoMode { get { return m_AutoMode; } }
+
+			internal ScriptWorker(EnhancedScript script, TimeSpan delay, bool autoMode)
 			{
 				m_Script = script;
+				m_Delay = delay;
+				m_AutoMode = autoMode;
 			}
 
 			// This method will be called when the thread is started.
 			internal void AsyncRun()
 			{
-				int exit = 0;
+				int exit = Int32.MinValue;
 
 				while (!_shouldStop)
 				{
 					Thread.Sleep(m_Delay);
-					exit = InvokeMethod<int>(m_Script, "Run");
-					if (exit != 0)
-						_shouldStop = true;
+
+					RunMode action;
+					if (m_Actions.Count > 0 && m_Actions.TryDequeue(out action))
+					{
+						exit = InvokeMethod<int>(m_Script, "Run", action);
+
+						if (this.AutoMode)
+						{
+							if (exit != 0)
+								_shouldStop = true;
+						}
+						else
+						{
+							if (exit != Int32.MinValue)
+								_shouldStop = true;
+						}
+					}
 				}
 
 				if (exit != 0)
@@ -72,8 +96,8 @@ namespace RazorEnhanced
 		private static List<ScriptWorker> m_Workers = new List<ScriptWorker>();
 		internal static List<ScriptWorker> Workers { get { return m_Workers; } }
 
-		private static List<Thread> m_Threads = new List<Thread>();
-		internal static List<Thread> Threads { get { return m_Threads; } }
+		private static List<Task> m_Tasks = new List<Task>();
+		internal static List<Task> Tasks { get { return m_Tasks; } }
 
 		private static PaxScripter m_PaxScripter;
 
@@ -100,7 +124,7 @@ namespace RazorEnhanced
 			get { return m_Auto; }
 			set
 			{
-				if (m_Threads.Count != m_Workers.Count)
+				if (m_Tasks.Count != m_Workers.Count)
 				{
 					MessageBox.Show("Thread count different from Worker count", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 					return;
@@ -108,27 +132,30 @@ namespace RazorEnhanced
 
 				if (value)
 				{
-					for (int i = 0; i < m_Threads.Count; i++)
+					for (int i = 0; i < m_Tasks.Count; i++)
 					{
-						Thread thread = m_Threads[i];
+						Task task = m_Tasks[i];
 						ScriptWorker worker = m_Workers[i];
 
-						// Start the worker thread.
-						thread.Start();
+						// Start the worker task.
+						task.Start();
 						// Loop until worker thread activates.
-						while (!thread.IsAlive) ;
+						while (task.Status == TaskStatus.Running)
+						{
+							Thread.Sleep(1);
+						}
 
 						Misc.SendMessage("Script " + worker.Script.Class + " started.");
 					}
 				}
 				else
 				{
-					for (int i = 0; i < m_Threads.Count; i++)
+					for (int i = 0; i < m_Tasks.Count; i++)
 					{
-						Thread thread = m_Threads[i];
+						Task task = m_Tasks[i];
 						ScriptWorker worker = m_Workers[i];
 
-						// Request that the worker thread stop itself:
+						// Request that the worker task stop itself:
 						worker.RequestStop();
 
 						Misc.SendMessage("Script " + worker.Script.Class + " stopped.");
@@ -143,22 +170,28 @@ namespace RazorEnhanced
 		{
 			Auto = false;
 			m_Workers.Clear();
-			m_Threads.Clear();
+			m_Tasks.Clear();
 			m_PaxScripter.Reset();
 		}
+
+		private static TimeSpan m_AutoDelay = TimeSpan.FromMilliseconds(25);
 
 		internal static string Load(string file)
 		{
 			string status = "Loaded";
 			string classname = Path.GetFileNameWithoutExtension(file);
+
 			EnhancedScript script = new EnhancedScript(file, classname);
-			ScriptWorker worker = new ScriptWorker(script);
-			Thread thread = new Thread(worker.AsyncRun);
+
+			ScriptWorker worker = new ScriptWorker(script, m_AutoDelay, true);
+			worker.Actions.Enqueue(RunMode.Run);
+
+			Task task = new Task(worker.AsyncRun);
 
 			try
 			{
 				m_Workers.Add(worker);
-				m_Threads.Add(thread);
+				m_Tasks.Add(task);
 				m_PaxScripter.AddModule(script.Class);
 				m_PaxScripter.AddCodeFromFile(script.Class, script.File);
 			}
@@ -170,12 +203,12 @@ namespace RazorEnhanced
 			return status;
 		}
 
-		private static T InvokeMethod<T>(EnhancedScript script, string method)
+		private static T InvokeMethod<T>(EnhancedScript script, string method, RunMode action)
 		{
 			T result = default(T);
 			try
 			{
-				result = (T)m_PaxScripter.Invoke(RunMode.Run, null, "RazorEnhanced." + script.Class + "." + method);
+				result = (T)m_PaxScripter.Invoke(action, null, "RazorEnhanced." + script.Class + "." + method);
 			}
 			catch
 			{
