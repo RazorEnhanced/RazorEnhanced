f147162390978c79fff5a11200951866eadcc8b9
magnetostaff <magnetostaff@yahoo.it>
2016-01-06 00:05:17 +0100

    fix cumulativi version 0.1.2: - fixata datagrid scritp possono essere editati solo i check box booleani - rimosso controllo NTP per expire date - riformattati alcuni script

--- Razor/Core/Main.cs
+++ Razor/Core/Main.cs
@@ -228,37 +228,6 @@ namespace Assistant
 
 		private static string m_Version;
 
-		private static DateTime GetNetworkTime()
-		{
-			try
-			{
-				const string ntpServer = "pool.ntp.org";
-				var ntpData = new byte[48];
-				ntpData[0] = 0x1B; //LeapIndicator = 0 (no warning), VersionNum = 3 (IPv4 only), Mode = 3 (Client Mode)
-
-				var addresses = Dns.GetHostEntry(ntpServer).AddressList;
-				var ipEndPoint = new IPEndPoint(addresses[0], 123);
-				var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-				socket.Connect(ipEndPoint);
-				socket.Send(ntpData);
-				socket.Receive(ntpData);
-				socket.Close();
-
-				ulong intPart = (ulong)ntpData[40] << 24 | (ulong)ntpData[41] << 16 | (ulong)ntpData[42] << 8 | (ulong)ntpData[43];
-				ulong fractPart = (ulong)ntpData[44] << 24 | (ulong)ntpData[45] << 16 | (ulong)ntpData[46] << 8 | (ulong)ntpData[47];
-
-				var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
-				var networkDateTime = (new DateTime(1900, 1, 1)).AddMilliseconds((long)milliseconds);
-
-				return networkDateTime;
-			}
-			catch (Exception ex)
-			{
-				return DateTime.MaxValue;
-			}
-		}
-
 		[STAThread]
 		public static void Main(string[] Args)
 		{
@@ -269,8 +238,7 @@ namespace Assistant
 				throw new InvalidOperationException("This Razor installation is corrupted.");
 
 			DateTime local = DateTime.Now;
-			DateTime ntp = GetNetworkTime();
-			if (local > m_ExpireDate || ntp > m_ExpireDate)
+			if (local > m_ExpireDate)
 			{
 				MessageBox.Show("This Razor installation has expired!", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
