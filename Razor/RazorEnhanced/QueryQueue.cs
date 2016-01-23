999f8284d5d271a7ebe8a7e9c54e0bfc8a0abbc0
Alexdan <dna.volpe@libero.it>
2016-01-23 02:34:54 +0100

    Rivisto queryqueue da thread a timer

--- Razor/RazorEnhanced/QueryQueue.cs
+++ Razor/RazorEnhanced/QueryQueue.cs
@@ -8,54 +8,43 @@ namespace RazorEnhanced
 	{
 		internal static ConcurrentQueue<int> QueryStats = new ConcurrentQueue<int>();
 		internal static ConcurrentQueue<int> QueryMobsProps = new ConcurrentQueue<int>();
-		private static Thread m_processqueuethread = new Thread(ProcessQueue);
+		private static System.Timers.Timer m_timer = new System.Timers.Timer();
 
 		internal static void Start()
 		{
 			QueryStats = new ConcurrentQueue<int>();
 			QueryMobsProps = new ConcurrentQueue<int>();
-			if (!m_processqueuethread.IsAlive)
-			{
-				m_processqueuethread = new Thread(ProcessQueue);
-				m_processqueuethread.Start();
-			}
-
+			m_timer.Enabled = true;
+			m_timer.Interval = 5;
+			m_timer.Elapsed += ProcessQueue;
 		}
 
-		internal static void ProcessQueue()
+		private static void ProcessQueue(object source, System.Timers.ElapsedEventArgs e)
 		{
-			while (true)
+			if (!QueryStats.IsEmpty)
 			{
-				if (!QueryStats.IsEmpty)
+				int s = 0;
+				QueryStats.TryDequeue(out s);
+				ClientCommunication.SendToServer(new StatusQuery(s));
+				Thread.Sleep(100);
+			}
+
+			if (World.Player.Expansion > 3)
+			{
+				if (!QueryMobsProps.IsEmpty)
 				{
 					int s = 0;
-					QueryStats.TryDequeue(out s);
-					ClientCommunication.SendToServer(new StatusQuery(s));
-					Thread.Sleep(100);
+					QueryMobsProps.TryDequeue(out s);
+					ClientCommunication.SendToServer(new QueryProperties(s));
 				}
-
-				if (World.Player.Expansion > 3)
-				{
-					if (!QueryMobsProps.IsEmpty)
-					{
-						int s = 0;
-						QueryMobsProps.TryDequeue(out s);
-						ClientCommunication.SendToServer(new QueryProperties(s));
-						Thread.Sleep(100);
-					}
-				}
-				Thread.Sleep(5);
 			}
+			Thread.Sleep(5);
+			RazorEnhanced.AutoLoot.AddLog("sss");
 		}
 
 		internal static void Abort()
 		{
-			try
-			{
-				if (m_processqueuethread != null && (m_processqueuethread.ThreadState == ThreadState.Running || m_processqueuethread.ThreadState == ThreadState.WaitSleepJoin))
-					m_processqueuethread.Abort();
-			}
-			catch { }
+			m_timer.Close();
 		}
 	}
 }
