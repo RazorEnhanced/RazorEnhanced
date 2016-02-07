e908cd0d487d69c98d4313d5fc1f5d6bd4ed3682
Alexdan <dna.volpe@libero.it>
2016-02-07 15:08:52 +0100

    aggiunti try catch su lettura journal

--- Razor/RazorEnhanced/Journal.cs
+++ Razor/RazorEnhanced/Journal.cs
@@ -31,61 +31,95 @@ namespace RazorEnhanced
 		public static void Clear()
 		{
 			World.Player.Journal.Clear();
-			//Misc.SendMessage("Journal Cleared");
 		}
 
 		public static bool Search(string text)
 		{
-			foreach (JournalEntry entrys in World.Player.Journal)
+			try
 			{
-				if (entrys.Text.Contains(text))
-					return true;
+				foreach (JournalEntry entrys in World.Player.Journal)
+				{
+					if (entrys.Text.Contains(text))
+						return true;
+				}
+				return false;
 			}
-			return false;
+			catch
+			{
+				return false;
+			}
 		}
 
 		public static bool SearchByName(string text, string name)
 		{
-			foreach (JournalEntry entrys in World.Player.Journal)
+			try
 			{
-				if (entrys.Name == name)
-					if (entrys.Text.Contains(text))
-						return true;
+				foreach (JournalEntry entrys in World.Player.Journal)
+				{
+					if (entrys.Name == name)
+						if (entrys.Text.Contains(text))
+							return true;
+				}
+				return false;
 			}
-			return false;
+			catch
+			{
+				return false;
+			}
 		}
 
 		public static bool SearchByColor(string text, int color)
 		{
-			foreach (JournalEntry entrys in World.Player.Journal)
+			try
 			{
-				if (entrys.Color == color)
-					if (entrys.Text.Contains(text))
-						return true;
+				foreach (JournalEntry entrys in World.Player.Journal)
+				{
+					if (entrys.Color == color)
+						if (entrys.Text.Contains(text))
+							return true;
+				}
+				return false;
 			}
-			return false;
+			catch
+			{
+				return false;
+			}
 		}
 
 		public static bool SearchByType(string text, string type)
 		{
-			foreach (JournalEntry entrys in World.Player.Journal)
+			try
 			{
-				if (entrys.Type.ToString() == type)
-					if (entrys.Text.Contains(text))
-						return true;
+				foreach (JournalEntry entrys in World.Player.Journal)
+				{
+					if (entrys.Type.ToString() == type)
+						if (entrys.Text.Contains(text))
+							return true;
+				}
+				return false;
 			}
-			return false;
+			catch
+			{
+				return false;
+			}
 		}
 
 		public static string GetLineText(string text)
 		{
 			string result = "";
-			foreach (JournalEntry entrys in World.Player.Journal)
+			try
 			{
-				if (entrys.Text.Contains(text))
-					result = entrys.Text;
+				foreach (JournalEntry entrys in World.Player.Journal)
+				{
+					if (entrys.Text.Contains(text))
+						result = entrys.Text;
+				}
+				return result;
 			}
-			return result;
+			catch
+			{
+				return result;
+			}
 		}
 
 		public static void WaitJournal(string text, int delay)
