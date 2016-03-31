0133bdc4ae2abf8cdc5b48049fe77fa3c4349eaf
Davide Iuffrida <david.iuffri94@hotmail.it>
2016-03-31 20:59:52 +0200

    PathFiding sperimentale parte 4 - Risistemato il codice, data più dinamicita

--- Razor/RazorEnhanced/PathFinding.cs
+++ Razor/RazorEnhanced/PathFinding.cs
@@ -61,50 +61,47 @@ namespace RazorEnhanced
 
 		public static void TestRoute(int startx, int starty, int endx, int endy)
 		{
-			int maxx = 0;
-			int minx = 0;
-			int maxy = 0;
-			int miny = 0;
+            //Calcolo l'asse più lungo
+		    int xAxis = Math.Abs(startx - endx);
+		    int yAxis = Math.Abs(starty - endy);
+		    int range = xAxis > yAxis ? xAxis + 20 : yAxis + 20;
+            int gridSize = range * 2;
 
-			//BaseGrid walkgrid = new StaticGrid(Math.Abs(startx - endx), Math.Abs(starty - endy));
-			BaseGrid walkgrid = new StaticGrid(200, 200);
+            BaseGrid walkgrid = new StaticGrid(gridSize, gridSize);
 
-			Misc.SendMessage("Grid dim: " + Math.Abs(startx - endx) + " " + Math.Abs(starty - endy));
-			if (startx > endx)
-			{
-				maxx = startx;
-				minx = endx;
-			}
-			else
-			{
-				maxx = endx;
-				minx = startx;
-			}
+			Misc.SendMessage("Grid dim: " + gridSize + "x" + gridSize);
 
-			if (starty > endy)
-			{
-				maxy = starty;
-				miny = endy;
-			}
-			else
-			{
-				maxy = endy;
-				miny = starty;
-			}
+            int minx = startx > endx ? endx : startx;
+            int maxx = startx > endx ? startx : endx;
+            int miny = starty > endy ? starty : endy;
+            int maxy = starty > endy ? endy : starty;
 
-			Misc.SendMessage("Max x: " + maxx + " Min x: " + minx);
-			Misc.SendMessage("Max y: " + maxy + " Min y: " + miny);
+			Misc.SendMessage("Min x: " + minx + " Max x: " + maxx);
+			Misc.SendMessage("Min y: " + miny + " Max y: " + maxy);
 
-			for (int x = 0; x < 200; x++)
+		    int mediumXPoint = (startx + endx)/2;
+		    int mediumYPoint = (starty + endy)/2;
+
+		    int countNonPass = 0;
+		    int countPass = 0;
+			for (int x = 0, xx = mediumXPoint - range; x < gridSize; x++, xx++)
 			{
-				for (int y = 0; y < 200; y++)
+				for (int y = 0, yy = mediumYPoint - range; y < gridSize; y++, yy++)
 				{
-					walkgrid.SetWalkableAt(x, y,true);
+				    bool isPassable = Passable(xx, yy);
+				    if (!isPassable)
+				        countNonPass++;
+				    else
+				        countPass++;
+					walkgrid.SetWalkableAt(x, y, isPassable);
 				}
 			}
 
-			GridPos startPos = new GridPos(100, 100);
-			GridPos endPos = new GridPos(startx - endx + 100, starty - endy + 100);
+            Misc.SendMessage("Amount non passabili: " + countNonPass);
+            Misc.SendMessage("Amount passabili: " + countPass);
+
+            GridPos startPos = new GridPos(range, range);
+			GridPos endPos = new GridPos(startx - endx + range, starty - endy + range);
 			JumpPointParam jpParam = new JumpPointParam(walkgrid, startPos, endPos, false, false, false);
 			List<GridPos> resultPathList = JumpPointFinder.FindPath(jpParam);
 
@@ -127,21 +124,21 @@ namespace RazorEnhanced
 						Assistant.Direction dd2 = World.Player.Direction;
 
 						if (xx <= -1 && yy <= -1)
-							RazorEnhanced.Player.Walk("Up");
+							RazorEnhanced.Player.Run("Up");
 						else if (xx <= -1 && yy == 0)
-							RazorEnhanced.Player.Walk("West");
+							RazorEnhanced.Player.Run("West");
 						else if (xx <= -1 && yy >= 1)
-							RazorEnhanced.Player.Walk("Left");
+							RazorEnhanced.Player.Run("Left");
 						else if (xx == 0 && yy >= 1)
-							RazorEnhanced.Player.Walk("South");
+							RazorEnhanced.Player.Run("South");
 						else if (xx >= 1 && yy >= 1)
-							RazorEnhanced.Player.Walk("Down");
+							RazorEnhanced.Player.Run("Down");
 						else if (xx >= 1 && yy == 0)
-							RazorEnhanced.Player.Walk("East");
+							RazorEnhanced.Player.Run("East");
 						else if (xx >= 1 && yy <= -1)
-							RazorEnhanced.Player.Walk("Right");
+							RazorEnhanced.Player.Run("Right");
 						else if (xx == 0 && yy <= -1)
-							RazorEnhanced.Player.Walk("North");
+							RazorEnhanced.Player.Run("North");
                         
 						while (pp2 == World.Player.Position && dd2 == World.Player.Direction)
 						{
@@ -152,8 +149,6 @@ namespace RazorEnhanced
 				}
 
 				RazorEnhanced.Misc.SendMessage("step -> X: " + step.x + " Y: " + step.y);
-
-				RazorEnhanced.Player.Walk("");
 			}
 		}
 	}
