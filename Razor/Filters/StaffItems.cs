da69fbf7f8460a9b51097d7dc40c7bf96b05be52
Alexdan <dna.volpe@libero.it>
2017-04-18 00:18:54 +0200

    Fix StaffItem per packet WorldItem SA

--- Razor/Filters/StaffItems.cs
+++ Razor/Filters/StaffItems.cs
@@ -11,7 +11,7 @@ namespace Assistant.Filters
 		{
 		}
 
-		internal override byte[] PacketIDs { get { return new byte[] { 0x1A }; } }
+		internal override byte[] PacketIDs { get { return new byte[] { 0x1A, 0xF3 }; } }
 
 		internal override LocString Name { get { return LocString.StaffOnlyItems; } }
 
@@ -30,36 +30,71 @@ namespace Assistant.Filters
 
 		internal override void OnFilter(PacketReader p, PacketHandlerEventArgs args)
 		{
-			uint serial = p.ReadUInt32();
-			ushort itemID = p.ReadUInt16();
+			if (p.PacketID == 0xF3) // SA World Item
+			{
+				p.ReadUInt16(); // Unknow 
 
-			if ((serial & 0x80000000) != 0)
+				byte artDataID = p.ReadByte(); // artID 
+
+				uint serial = p.ReadUInt32();
+				ushort itemID = p.ReadUInt16();
+				itemID = (ushort)(artDataID == 0x02 ? itemID | 0x4000 : itemID);
+
+				p.ReadByte(); //Direction
+
 				p.ReadUInt16(); // amount
+				p.ReadUInt16(); // amount 2
 
-			if ((itemID & 0x8000) != 0)
-				itemID = (ushort)((itemID & 0x7FFF) + p.ReadSByte()); // itemID offset
+				p.ReadUInt16(); // x
+				p.ReadUInt16(); // y
+				p.ReadSByte(); // z
 
-			ushort x = p.ReadUInt16();
-			ushort y = p.ReadUInt16();
+				p.ReadByte(); // Light
 
-			if ((x & 0x8000) != 0)
-				p.ReadByte(); // direction
-
-			short z = p.ReadSByte();
-
-			if ((y & 0x8000) != 0)
 				p.ReadUInt16(); // hue
 
-			bool visable = true;
-			if ((y & 0x4000) != 0)
-			{
+				bool visable = true;
+
 				int flags = p.ReadByte();
 
 				visable = ((flags & 0x80) == 0);
-			}
 
-			if (IsStaffItem(itemID) || !visable)
-				args.Block = true;
+				if (IsStaffItem(itemID) || !visable)
+					args.Block = true;
+			}
+			else
+			{
+				uint serial = p.ReadUInt32();
+				ushort itemID = p.ReadUInt16();
+
+				if ((serial & 0x80000000) != 0)
+					p.ReadUInt16(); // amount
+
+				if ((itemID & 0x8000) != 0)
+					itemID = (ushort)((itemID & 0x7FFF) + p.ReadSByte()); // itemID offset
+
+				ushort x = p.ReadUInt16();
+				ushort y = p.ReadUInt16();
+
+				if ((x & 0x8000) != 0)
+					p.ReadByte(); // direction
+
+				short z = p.ReadSByte();
+
+				if ((y & 0x8000) != 0)
+					p.ReadUInt16(); // hue
+
+				bool visable = true;
+				if ((y & 0x4000) != 0)
+				{
+					int flags = p.ReadByte();
+
+					visable = ((flags & 0x80) == 0);
+				}
+
+				if (IsStaffItem(itemID) || !visable)
+					args.Block = true;
+			}
 		}
 
 		internal override void OnEnable()
