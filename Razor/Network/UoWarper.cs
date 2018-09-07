026e2d68dd363b08cb2039afede1b7a37cee5ec5
DESKTOP-JGMII7N\Mimmo <dnavolpe@libero.it>
2018-09-07 19:46:09 +0200

    Revert "New Pathfind part Two: Now can run in multiple client. Thx Soltrac for help"
    This reverts commit a69b434e3eb385f4411f57a7341f711ccc12a99f.

---
+++ Razor/Network/UoWarper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Assistant;
+
+namespace RazorEnhanced
+{
+	internal class UoWarper
+	{
+		internal static UO UODLLHandleClass = null;
+
+		private static IntPtr _UOHandle = DLLImport.Uo.Open();
+
+		internal class UO
+		{
+			internal bool Open(int clinr = 1)
+			{
+				_UOHandle = DLLImport.Uo.Open();
+				if (DLLImport.Uo.Version() != 3)
+				{
+					return false;
+				}
+				DLLImport.Uo.SetTop(_UOHandle, 0);
+				DLLImport.Uo.PushStrVal(_UOHandle, "Set");
+				DLLImport.Uo.PushStrVal(_UOHandle, "CliNr");
+				DLLImport.Uo.PushInteger(_UOHandle, clinr);
+				return DLLImport.Uo.Execute(_UOHandle) == 0;
+			}
+
+			internal void Pathfind(int X, int Y, int Z)
+			{
+				this._executeCommand(false, "Pathfind", new object[]
+				{
+				X,
+				Y,
+				Z
+				});
+			}
+
+			internal List<object> _executeCommand(bool ReturnResults, string CommandName, object[] args)
+			{
+				List<object> Results = new List<object>();
+				DLLImport.Uo.SetTop(_UOHandle, 0);
+				DLLImport.Uo.PushStrVal(_UOHandle, "Call");
+				DLLImport.Uo.PushStrVal(_UOHandle, CommandName);
+				for (int j = 0; j < args.Length; j++)
+				{
+					object o = args[j];
+					if (o is int)
+					{
+						DLLImport.Uo.PushInteger(_UOHandle, Convert.ToInt32(o));
+					}
+					else if (o is string)
+					{
+						DLLImport.Uo.PushStrVal(_UOHandle, Convert.ToString(o));
+					}
+					else if (o is bool)
+					{
+						DLLImport.Uo.PushBoolean(_UOHandle, Convert.ToBoolean(o));
+					}
+					else if (o is IntPtr)
+					{
+						DLLImport.Uo.PushPointer(_UOHandle, (IntPtr)o);
+					}
+				}
+				if (DLLImport.Uo.Execute(_UOHandle) != 0)
+				{
+					return null;
+				}
+				if (!ReturnResults)
+				{
+					return null;
+				}
+				int objectcnt = DLLImport.Uo.GetTop(_UOHandle);
+				for (int i = 1; i <= objectcnt; i++)
+				{
+					switch (DLLImport.Uo.GetType(_UOHandle, i))
+					{
+						case 1:
+							Results.Add(DLLImport.Uo.GetBoolean(_UOHandle, i).ToString());
+							break;
+						case 2:
+							Results.Add(DLLImport.Uo.GetPointer(_UOHandle, i));
+							break;
+						case 3:
+							Results.Add(DLLImport.Uo.GetInteger(_UOHandle, i).ToString());
+							break;
+						case 4:
+							Results.Add(Marshal.PtrToStringAnsi(DLLImport.Uo.GetString(_UOHandle, i)));
+							break;
+						default:
+							throw new NotImplementedException();
+					}
+				}
+				return Results;
+			}		
+		}
+	}
+}
--- Razor/RazorEnhanced/Player.cs
+++ Razor/RazorEnhanced/Player.cs
@@ -1042,14 +1042,84 @@ namespace RazorEnhanced
 
 		public static void PathFindTo(int x, int y, int z)
 		{
-			if (ClientCommunication.PathFindAddress == 0 || ClientCommunication.E_OLDDIRAddress == 0 || ClientCommunication.E_REDIRAddress == 0)
+			Mutex m;
+			if (!Mutex.TryOpenExisting("mutexrazorenhanced", out m))
 			{
-				Misc.SendMessage("Can't detect pathfind function for your client version, Contact support on discord channel.");
-				return;
+				m = new Mutex(true, "mutexrazorenhanced");
+				//m.WaitOne();
 			}
+			else
+			{
+				try  // AbandonedMutexException
+				{
+					m.WaitOne();
+				}
+				catch
+				{
+					m = new Mutex(true, "mutexrazorenhanced");
+					//m.WaitOne();
+				}
+			}
+			UoWarper.UODLLHandleClass = new RazorEnhanced.UoWarper.UO();
+			while (!UoWarper.UODLLHandleClass.Open())
+			{
+				Thread.Sleep(50);
+			}
+			UoWarper.UODLLHandleClass.Pathfind(x, y, z);
+			UoWarper.UODLLHandleClass.Close();
 
-			MemoryComm memoryComm = new MemoryComm();
-			memoryComm.PathFind(Convert.ToInt16(x), Convert.ToInt16(y), Convert.ToInt16(z));
+			try  // AbandonedMutexException
+			{
+				m.ReleaseMutex();
+			}
+			catch {
+
+			}
+			Misc.Pause(70);
+			
+
+			/*if (!Mutex.TryOpenExisting("mutexrazorenhanced", out UoWarper.UoModMutex))
+			{
+				UoWarper.UoModMutex = new Mutex(true, "mutexrazorenhanced");
+				RazorEnhanced.AutoLoot.AddLog("Creo nuovo");
+			}
+			else
+			{
+				try  // AbandonedMutexException
+				{
+					RazorEnhanced.AutoLoot.AddLog("wait");
+					UoWarper.UoModMutex.WaitOne(2000);
+					RazorEnhanced.AutoLoot.AddLog("END wait");
+				}
+				catch
+				{
+					RazorEnhanced.AutoLoot.AddLog("wait NEW");
+					UoWarper.UoModMutex = new Mutex(true, "mutexrazorenhanced");
+					UoWarper.UoModMutex.WaitOne(2000);
+					RazorEnhanced.AutoLoot.AddLog("END NEW");
+				}
+				
+				
+			}
+			
+			UoWarper.UODLLHandleClass = new RazorEnhanced.UoWarper.UO();
+			while (!UoWarper.UODLLHandleClass.Open())
+			{
+				Thread.Sleep(10);
+			}
+			UoWarper.UODLLHandleClass.Pathfind(x, y, z);
+			Thread.Sleep(100);
+
+			UoWarper.UoModMutex.ReleaseMutex();
+			/*
+			/*
+			UoWarper.UODLLHandleClass = new RazorEnhanced.UoWarper.UO();
+			while (!UoWarper.UODLLHandleClass.Open())
+			{
+				Thread.Sleep(10);
+			}
+			UoWarper.UODLLHandleClass.Pathfind(x, y, z);
+			*/
 		}
 
 		internal static void PathFindToPacket(Assistant.Point3D Location)
