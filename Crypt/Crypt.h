ca4df4e05d1000da8e39bc4bcf118e72877b5997
DESKTOP-JGMII7N\Mimmo <dnavolpe@libero.it>
2018-09-19 12:23:31 +0200

    Release "0.6.49"

--- Crypt/Crypt.h
+++ Crypt/Crypt.h
@@ -1,7 +1,7 @@
 #pragma once
 #pragma pack(1)
 
-#define DLL_VERSION "0.6.48"
+#define DLL_VERSION "0.6.49"
 #define DLL_VERSION_OLD "1.0.14"  // Not change IT!
 
 #define DLLFUNCTION __declspec(dllexport)
