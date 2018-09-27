c3876461da62982baff9bb6767728c2f64c0fd03
DESKTOP-JGMII7N\Mimmo <dnavolpe@libero.it>
2018-09-27 21:34:07 +0200

    Release 0.6.52

--- Razor/Properties/AssemblyInfo.cs
+++ Razor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//
+// 
 [assembly: AssemblyTitle("Razor Enhanced")]
 [assembly: AssemblyDescription("The bleeding edge UO tool")]
 [assembly: AssemblyConfiguration("")]
