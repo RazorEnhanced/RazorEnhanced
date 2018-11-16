0526e5b902c68356b1a43e03ad3f66d929c31e28
DESKTOP-JGMII7N\Mimmo <dnavolpe@libero.it>
2018-11-16 20:35:13 +0100

    Reordered code of GUI agent in different file

---
+++ Razor/UI/BandageHeal.cs
@@ -0,0 +1,244 @@
+﻿using Assistant.Filters;
+using RazorEnhanced;
+using RazorEnhanced.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq;
+using Accord.Video.DirectShow;
+using AutoUpdaterDotNET;
+
+
+namespace Assistant
+{
+	internal partial class MainForm : System.Windows.Forms.Form
+	{
+		internal ListBox BandageHealLogBox { get { return bandagehealLogBox; } }
+		internal RazorCheckBox BandageHealenableCheckBox { get { return bandagehealenableCheckBox; } }
+		internal RazorComboBox BandageHealtargetComboBox { get { return bandagehealtargetComboBox; } }
+		internal Label BandageHealtargetLabel { get { return bandagehealtargetLabel; } }
+		internal RazorCheckBox BandageHealcustomCheckBox { get { return bandagehealcustomCheckBox; } }
+		internal RazorAgentNumHexTextBox BandageHealcustomIDTextBox { get { return bandagehealcustomIDTextBox; } }
+		internal RazorAgentNumHexTextBox BandageHealcustomcolorTextBox { get { return bandagehealcustomcolorTextBox; } }
+		internal RazorCheckBox BandageHealdexformulaCheckBox { get { return bandagehealdexformulaCheckBox; } }
+		internal RazorAgentNumOnlyTextBox BandageHealdelayTextBox { get { return bandagehealdelayTextBox; } }
+		internal RazorAgentNumOnlyTextBox BandageHealhpTextBox { get { return bandagehealhpTextBox; } }
+		internal RazorAgentNumOnlyTextBox BandageHealMaxRangeTextBox { get { return bandagehealmaxrangeTextBox; } }
+		internal RazorCheckBox BandageHealpoisonCheckBox { get { return bandagehealpoisonCheckBox; } }
+		internal RazorCheckBox BandageHealmortalCheckBox { get { return bandagehealmortalCheckBox; } }
+		internal RazorCheckBox BandageHealhiddedCheckBox { get { return bandagehealhiddedCheckBox; } }
+		internal RazorCheckBox BandageHealcountdownCheckBox { get { return bandagehealcountdownCheckBox; } }
+		internal RazorButton BandageHealsettargetButton { get { return bandagehealsettargetButton; } }
+		internal RazorCheckBox BandageHealAutostartCheckBox { get { return bandagehealAutostartCheckBox; } }
+
+		private void bandagehealenableCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (World.Player != null)
+			{
+				if (bandagehealenableCheckBox.Checked)
+				{
+					RazorEnhanced.BandageHeal.AutoMode = true;
+					RazorEnhanced.BandageHeal.AddLog("BANDAGE HEAL: Engine Start...");
+					if (showagentmessageCheckBox.Checked)
+						RazorEnhanced.Misc.SendMessage("BANDAGE HEAL: Engine Start...", false);
+				}
+				else
+				{
+					// Stop BANDAGEHEAL
+					RazorEnhanced.BandageHeal.AutoMode = false;
+					if (showagentmessageCheckBox.Checked)
+						RazorEnhanced.Misc.SendMessage("BANDAGE HEAL: Engine Stop...", false);
+					RazorEnhanced.BandageHeal.AddLog("BANDAGE HEAL: Engine Stop...");
+				}
+			}
+			else
+			{
+				bandagehealenableCheckBox.Checked = false;
+				RazorEnhanced.BandageHeal.AddLog("You are not logged in game!");
+			}
+
+			if (bandagehealenableCheckBox.Checked)
+				groupBox6.Enabled = false;
+			else
+				groupBox6.Enabled = true;
+		}
+		private void bandagehealAutostartCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealAutostartCheckBox.Focused)
+				Settings.General.WriteBool("BandageHealAutostartCheckBox", bandagehealAutostartCheckBox.Checked);
+		}
+		private void bandagehealtargetComboBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (BandageHealtargetComboBox.Text == "Target")
+			{
+				bandagehealsettargetButton.Enabled = true;
+				bandagehealtargetLabel.Enabled = true;
+			}
+			else
+			{
+				bandagehealsettargetButton.Enabled = false;
+				bandagehealtargetLabel.Enabled = false;
+			}
+
+			RazorEnhanced.Settings.General.WriteString("BandageHealtargetComboBox", bandagehealtargetComboBox.Text);
+		}
+
+		private void bandagehealsettargetButton_Click(object sender, EventArgs e)
+		{
+			Targeting.OneTimeTarget(new Targeting.TargetResponseCallback(BandageHeakMobileTarget_Callback));
+		}
+
+		private void BandageHeakMobileTarget_Callback(bool loc, Assistant.Serial serial, Assistant.Point3D pt, ushort itemid)
+		{
+			Assistant.Mobile mobile = Assistant.World.FindMobile(serial);
+
+			if (mobile == null)
+			{
+				if (showagentmessageCheckBox.Checked)
+					RazorEnhanced.Misc.SendMessage("Invalid Target!", false);
+				RazorEnhanced.BandageHeal.AddLog("Invalid Target!");
+				return;
+			}
+
+			if (mobile.Serial.IsMobile)
+			{
+				if (showagentmessageCheckBox.Checked)
+					RazorEnhanced.Misc.SendMessage("Bandage Heal target set to: " + mobile.Name, false);
+				RazorEnhanced.BandageHeal.AddLog("Bandage Heal target set to: " + mobile.Name);
+				BandageHeal.TargetSerial = mobile.Serial;
+				RazorEnhanced.Settings.General.WriteInt("BandageHealtargetLabel", mobile.Serial);
+			}
+			else
+			{
+				if (showagentmessageCheckBox.Checked)
+					RazorEnhanced.Misc.SendMessage("Invalid Target!", false);
+				RazorEnhanced.Scavenger.AddLog("Invalid Target!");
+			}
+		}
+
+		private void bandagehealcustomCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealcustomCheckBox.Checked)
+			{
+				bandagehealcustomIDTextBox.Enabled = true;
+				bandagehealcustomcolorTextBox.Enabled = true;
+			}
+			else
+			{
+				bandagehealcustomIDTextBox.Enabled = false;
+				bandagehealcustomcolorTextBox.Enabled = false;
+			}
+
+			if (bandagehealcustomCheckBox.Focused)
+				Settings.General.WriteBool("BandageHealcustomCheckBox", bandagehealcustomCheckBox.Checked);
+		}
+
+		private void bandagehealcustomIDTextBox_Leave(object sender, EventArgs e)
+		{
+			int id = -1;
+			try
+			{
+				id = Convert.ToInt32(bandagehealcustomIDTextBox.Text, 16);
+			}
+			catch { }
+
+			if (bandagehealcustomIDTextBox.Text == String.Empty || id == -1)
+			{
+				id = 0;
+				bandagehealcustomIDTextBox.Text = "0x0000";
+			}
+
+			RazorEnhanced.BandageHeal.CustomID = id;
+			RazorEnhanced.Settings.General.WriteInt("BandageHealcustomIDTextBox", id);
+		}
+
+		private void bandagehealcustomcolorTextBox_Leave(object sender, EventArgs e)
+		{
+			int color = -1;
+			try
+			{
+				color = Convert.ToInt32(bandagehealcustomcolorTextBox.Text, 16);
+			}
+			catch { }
+
+			if (bandagehealcustomcolorTextBox.Text == String.Empty || color == -1)
+			{
+				color = 0;
+				bandagehealcustomcolorTextBox.Text = "0x0000";
+			}
+
+			RazorEnhanced.BandageHeal.CustomColor = color;
+			RazorEnhanced.Settings.General.WriteInt("BandageHealcustomcolorTextBox", color);
+		}
+
+		private void bandagehealdexformulaCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealdexformulaCheckBox.Checked)
+				bandagehealdelayTextBox.Enabled = false;
+			else
+				bandagehealdelayTextBox.Enabled = true;
+
+			if (bandagehealdexformulaCheckBox.Focused)
+				RazorEnhanced.Settings.General.WriteBool("BandageHealdexformulaCheckBox", bandagehealdexformulaCheckBox.Checked);
+		}
+
+		private void bandagehealpoisonCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealpoisonCheckBox.Focused)
+				RazorEnhanced.Settings.General.WriteBool("BandageHealpoisonCheckBox", bandagehealpoisonCheckBox.Checked);
+		}
+
+		private void bandagehealmortalCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealmortalCheckBox.Focused)
+				RazorEnhanced.Settings.General.WriteBool("BandageHealmortalCheckBox", bandagehealmortalCheckBox.Checked);
+		}
+
+		private void bandagehealhiddedCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealhiddedCheckBox.Focused)
+				RazorEnhanced.Settings.General.WriteBool("BandageHealhiddedCheckBox", bandagehealhiddedCheckBox.Checked);
+		}
+
+		private void bandagehealcountdownCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (bandagehealcountdownCheckBox.Focused)
+				RazorEnhanced.Settings.General.WriteBool("BandageHealcountdownCheckBox", bandagehealcountdownCheckBox.Checked);
+		}
+
+		private void bandagehealdelayTextBox_Leave(object sender, EventArgs e)
+		{
+			if (bandagehealdelayTextBox.Text == String.Empty)
+				bandagehealdelayTextBox.Text = "1000";
+
+			BandageHeal.CustomDelay = Convert.ToInt32(bandagehealdelayTextBox.Text);
+			Settings.General.WriteInt("BandageHealdelayTextBox", BandageHeal.CustomDelay);
+		}
+
+		private void bandagehealhpTextBox_Leave(object sender, EventArgs e)
+		{
+			if (bandagehealhpTextBox.Text == String.Empty)
+				bandagehealhpTextBox.Text = "100";
+
+			BandageHeal.HpLimit = Convert.ToInt32(bandagehealhpTextBox.Text);
+			Settings.General.WriteInt("BandageHealhpTextBox", BandageHeal.HpLimit);
+		}
+
+		private void bandagehealmaxrangeTextBox_Leave(object sender, EventArgs e)
+		{
+			if (bandagehealmaxrangeTextBox.Text == String.Empty)
+				bandagehealmaxrangeTextBox.Text = "1";
+
+			BandageHeal.MaxRange = Convert.ToInt32(bandagehealmaxrangeTextBox.Text);
+			Settings.General.WriteInt("BandageHealMaxRangeTextBox", BandageHeal.MaxRange);
+		}
+	}
+}
---
+++ Razor/UI/Dress.cs
@@ -0,0 +1,360 @@
+﻿using Assistant.Filters;
+using RazorEnhanced;
+using RazorEnhanced.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq;
+using Accord.Video.DirectShow;
+using AutoUpdaterDotNET;
+
+
+namespace Assistant
+{
+	internal partial class MainForm : System.Windows.Forms.Form
+	{
+		internal CheckBox DressCheckBox { get { return dressConflictCheckB; } }
+		internal ListView DressListView { get { return dressListView; } }
+		internal ListBox DressLogBox { get { return dressLogBox; } }
+		internal RazorAgentNumOnlyTextBox DressDragDelay { get { return dressDragDelay; } }
+		internal ComboBox DressListSelect { get { return dressListSelect; } }
+		internal Label DressBagLabel { get { return dressBagLabel; } }
+
+		internal Button DressExecuteButton { get { return dressExecuteButton; } }
+		internal Button UnDressExecuteButton { get { return undressExecuteButton; } }
+		internal Button DressStopButton { get { return dressStopButton; } }
+
+		private void dressListSelect_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			RazorEnhanced.Dress.UpdateListParam(dressListSelect.Text);
+
+			if (dressListSelect.Focused && dressListSelect.Text != String.Empty)
+			{
+				Settings.Dress.ListUpdate(dressListSelect.Text, RazorEnhanced.Dress.DressDelay, RazorEnhanced.Dress.DressBag, RazorEnhanced.Dress.DressConflict, true);
+				RazorEnhanced.Dress.AddLog("Dress list changed to: " + dressListSelect.Text);
+			}
+
+			RazorEnhanced.Dress.RefreshItems();
+		}
+
+		private void dressAddListB_Click(object sender, EventArgs e)
+		{
+			foreach (Form f in Application.OpenForms)
+			{
+				if (f is EnhancedAgentAddList af)
+				{
+					af.AgentID = 6;
+					af.Focus();
+					return;
+				}
+			}
+			new EnhancedAgentAddList(6).Show();
+		}
+
+		private void dressRemoveListB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+			{
+				DialogResult dialogResult = MessageBox.Show("Are you sure to delete this Dress list: " + dressListSelect.Text, "Delete Dress List?", MessageBoxButtons.YesNo);
+				if (dialogResult == DialogResult.Yes)
+				{
+					RazorEnhanced.Dress.AddLog("Dress list " + dressListSelect.Text + " removed!");
+					RazorEnhanced.Dress.DressBag = 0;
+					RazorEnhanced.Dress.DressDelay = 100;
+					RazorEnhanced.Dress.DressConflict = false;
+					RazorEnhanced.Dress.RemoveList(dressListSelect.Text);
+					HotKey.Init();
+				}
+			}
+		}
+
+		private void dressDragDelay_Leave(object sender, EventArgs e)
+		{
+			if (dressDragDelay.Text == String.Empty)
+				dressDragDelay.Text = "100";
+
+			RazorEnhanced.Dress.DressDelay = Convert.ToInt32(dressDragDelay.Text);
+
+			RazorEnhanced.Settings.Dress.ListUpdate(dressListSelect.Text, RazorEnhanced.Dress.DressDelay, RazorEnhanced.Dress.DressBag, RazorEnhanced.Dress.DressConflict, true);
+			RazorEnhanced.Dress.RefreshLists();
+		}
+
+		private void dressConflictCheckB_CheckedChanged(object sender, EventArgs e)
+		{
+			if (dressConflictCheckB.Focused)
+			{
+				RazorEnhanced.Dress.DressConflict = dressConflictCheckB.Checked;
+				RazorEnhanced.Settings.Dress.ListUpdate(dressListSelect.Text, RazorEnhanced.Dress.DressDelay, RazorEnhanced.Dress.DressBag, RazorEnhanced.Dress.DressConflict, true);
+				RazorEnhanced.Dress.RefreshLists();
+			}
+		}
+
+		private void dressReadB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+				RazorEnhanced.Dress.ReadPlayerDress();
+			else
+				RazorEnhanced.Dress.AddLog("Item list not selected!");
+		}
+
+		private void dressSetBagB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+				Targeting.OneTimeTarget(new Targeting.TargetResponseCallback(DressItemContainerTarget_Callback));
+			else
+				RazorEnhanced.Dress.AddLog("Item list not selected!");
+		}
+
+		private void DressItemContainerTarget_Callback(bool loc, Assistant.Serial serial, Assistant.Point3D pt, ushort itemid)
+		{
+			Assistant.Item dressBag = Assistant.World.FindItem(serial);
+
+			if (dressBag == null)
+				return;
+
+			if (dressBag != null && dressBag.Serial.IsItem && dressBag.IsContainer)
+			{
+				if (showagentmessageCheckBox.Checked)
+					RazorEnhanced.Misc.SendMessage("Undress Container set to: " + dressBag.ToString(), false);
+				RazorEnhanced.Dress.AddLog("Undress Container set to: " + dressBag.ToString());
+				RazorEnhanced.Dress.DressBag = (int)dressBag.Serial.Value;
+			}
+			else
+			{
+				if (showagentmessageCheckBox.Checked)
+					RazorEnhanced.Misc.SendMessage("Invalid Undress Container, set backpack", false);
+				RazorEnhanced.Dress.AddLog("Invalid Undress Container, set backpack");
+				RazorEnhanced.Dress.DressBag = (int)World.Player.Backpack.Serial.Value;
+			}
+
+			this.BeginInvoke((MethodInvoker)delegate {
+				RazorEnhanced.Settings.Dress.ListUpdate(dressListSelect.Text, RazorEnhanced.Dress.DressDelay, RazorEnhanced.Dress.DressBag, RazorEnhanced.Dress.DressConflict, true);
+				RazorEnhanced.Dress.RefreshLists();
+			});
+		}
+
+		private void dressRemoveB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+			{
+				if (dressListView.SelectedItems.Count == 1)
+				{
+					int index = dressListView.SelectedItems[0].Index;
+					string selection = dressListSelect.Text;
+
+					if (RazorEnhanced.Settings.Dress.ListExists(selection))
+					{
+						List<Dress.DressItemNew> items = Settings.Dress.ItemsRead(selection);
+						if (index <= items.Count - 1)
+						{
+							RazorEnhanced.Settings.Dress.ItemDelete(selection, items[index]);
+							RazorEnhanced.Dress.RefreshItems();
+						}
+					}
+				}
+			}
+			else
+				RazorEnhanced.AutoLoot.AddLog("Item list not selected!");
+		}
+
+		private void dresslistView_ItemChecked(object sender, ItemCheckedEventArgs e)
+		{
+			if (dressListView.FocusedItem != null)
+			{
+				ListViewItem item = e.Item as ListViewItem;
+				RazorEnhanced.Dress.UpdateSelectedItems(item.Index);
+			}
+		}
+
+		private void dressAddTargetB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+				Targeting.OneTimeTarget(new Targeting.TargetResponseCallback(DressItemTarget_Callback));
+			else
+				RazorEnhanced.Dress.AddLog("Item list not selected!");
+		}
+
+		private void DressItemTarget_Callback(bool loc, Assistant.Serial serial, Assistant.Point3D pt, ushort itemid)
+		{
+			Assistant.Item dressItem = Assistant.World.FindItem(serial);
+			if (dressItem != null && dressItem.Serial.IsItem)
+				this.BeginInvoke((MethodInvoker)delegate { RazorEnhanced.Dress.AddItemByTarger(dressItem); });
+			else
+			{
+				if (showagentmessageCheckBox.Checked)
+					RazorEnhanced.Misc.SendMessage("Invalid target", false);
+				RazorEnhanced.Dress.AddLog("Invalid target");
+			}
+		}
+
+		private void dressAddManualB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+			{
+				EnhancedDressAddUndressLayer ManualAddLayer = new EnhancedDressAddUndressLayer
+				{
+					TopMost = true
+				};
+				ManualAddLayer.Show();
+			}
+			else
+				RazorEnhanced.Dress.AddLog("Item list not selected!");
+		}
+
+		private void dressImportListB_Click(object sender, EventArgs e)
+		{
+			RazorEnhanced.ImportExport.ImportDress();
+		}
+
+		private void dressExportListB_Click(object sender, EventArgs e)
+		{
+			if (dressListSelect.Text != String.Empty)
+				RazorEnhanced.ImportExport.ExportDress(dressListSelect.Text);
+			else
+				RazorEnhanced.Dress.AddLog("Item list not selected!");
+		}
+
+		private void razorButton10_Click(object sender, EventArgs e)
+		{
+			UndressStart();
+		}
+
+		internal void UndressStart()
+		{
+			if (World.Player == null) // non loggato
+			{
+				RazorEnhanced.Dress.AddLog("You are not logged in game!");
+				UndressFinishWork();
+				return;
+			}
+
+			if (dressListSelect.Text == String.Empty)
+			{
+				RazorEnhanced.Dress.AddLog("Item List not selected!");
+				UndressFinishWork();
+				return;
+			}
+
+			UndressStartWork();
+			RazorEnhanced.Dress.UndressStart();
+			RazorEnhanced.Organizer.AddLog("Undress Engine Start...");
+
+			if (showagentmessageCheckBox.Checked)
+				RazorEnhanced.Misc.SendMessage("UNDRESS: Engine Start...", false);
+		}
+
+		private delegate void UndressFinishWorkCallback();
+
+		internal void UndressFinishWork()
+		{
+			if (dressConflictCheckB.InvokeRequired ||
+				dressExecuteButton.InvokeRequired ||
+				undressExecuteButton.InvokeRequired ||
+				dressAddListB.InvokeRequired ||
+				dressRemoveListB.InvokeRequired ||
+				organizerExportListB.InvokeRequired ||
+				organizerImportListB.InvokeRequired ||
+				dressStopButton.InvokeRequired ||
+				organizerDragDelay.InvokeRequired)
+			{
+				UndressFinishWorkCallback d = new UndressFinishWorkCallback(UndressFinishWork);
+				this.Invoke(d, null);
+			}
+			else
+			{
+				dressStopButton.Enabled = false;
+				dressConflictCheckB.Enabled = true;
+				dressExecuteButton.Enabled = true;
+				undressExecuteButton.Enabled = true;
+				dressAddListB.Enabled = true;
+				dressRemoveListB.Enabled = true;
+				dressExportListB.Enabled = true;
+				dressImportListB.Enabled = true;
+				dressDragDelay.Enabled = true;
+			}
+		}
+
+		private delegate void UndressStartWorkCallback();
+
+		internal void UndressStartWork()
+		{
+			if (dressConflictCheckB.InvokeRequired ||
+				dressExecuteButton.InvokeRequired ||
+				undressExecuteButton.InvokeRequired ||
+				dressAddListB.InvokeRequired ||
+				dressRemoveListB.InvokeRequired ||
+				organizerExportListB.InvokeRequired ||
+				organizerImportListB.InvokeRequired ||
+				dressStopButton.InvokeRequired ||
+				organizerDragDelay.InvokeRequired)
+			{
+				UndressStartWorkCallback d = new UndressStartWorkCallback(UndressStartWork);
+				this.Invoke(d, null);
+			}
+			else
+			{
+				dressStopButton.Enabled = true;
+				dressConflictCheckB.Enabled = false;
+				dressExecuteButton.Enabled = false;
+				undressExecuteButton.Enabled = false;
+				dressAddListB.Enabled = false;
+				dressRemoveListB.Enabled = false;
+				dressExportListB.Enabled = false;
+				dressImportListB.Enabled = false;
+				dressDragDelay.Enabled = false;
+			}
+		}
+
+		private void dressExecuteButton_Click(object sender, EventArgs e)
+		{
+			DressStart();
+		}
+
+		internal void DressStart()
+		{
+			if (World.Player == null) // non loggato
+			{
+				RazorEnhanced.Dress.AddLog("You are not logged in game!");
+				UndressFinishWork();
+				return;
+			}
+
+			if (dressListSelect.Text == String.Empty)
+			{
+				RazorEnhanced.Dress.AddLog("Item List not selected!");
+				UndressFinishWork();
+				return;
+			}
+
+			UndressStartWork();
+			RazorEnhanced.Dress.DressStart();
+			RazorEnhanced.Organizer.AddLog("Dress Engine Start...");
+
+			if (showagentmessageCheckBox.Checked)
+				RazorEnhanced.Misc.SendMessage("DRESS: Engine Start...", false);
+		}
+
+		private void dressStopButton_Click(object sender, EventArgs e)
+		{
+			DressStop();
+		}
+
+		internal void DressStop()
+		{
+			RazorEnhanced.Dress.ForceStop();
+
+			RazorEnhanced.Dress.AddLog("Dress / Undress Engine force stop...");
+			if (showagentmessageCheckBox.Checked)
+				RazorEnhanced.Misc.SendMessage("DRESS/UNDRESS: Engine force stop...", false);
+			UndressFinishWork();
+		}
+	}
+}
