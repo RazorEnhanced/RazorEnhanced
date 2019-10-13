fb6cbd5ba9ee06d361d96da5e63880c8ec02d8ed
Credzba <credzba@gmail.com>
2019-10-13 11:39:59 -0500

    add 0.6.63 zip

--- Razor/Properties/AssemblyInfo.cs
+++ Razor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-// 
+//
 [assembly: AssemblyTitle("Razor Enhanced")]
 [assembly: AssemblyDescription("The bleeding edge UO tool")]
 [assembly: AssemblyConfiguration("")]
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.6.62.0")]
+[assembly: AssemblyVersion("0.6.63.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
