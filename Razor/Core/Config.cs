8f84d129116aa090a73ac3dd250b2f54cc2f090e
Cesare Montresor <cesare.montresor@gmail.com>
2021-04-15 02:01:18 +0200

    - added latest version of AutoDoc with stub of python stub generation. - added stub of Config with simple example for loading door data. - added all configfiles from UOS in json ( some structures can be simplified, did it for doors )

---
+++ Razor/Core/Config.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorEnhanced
+{
+    class Config
+    {
+        public static readonly string PATH_CONFIG_DOORS = "./Config/doors.json";
+        public static Doors Doors;
+
+
+
+
+        public static void load() {
+            try
+            {
+                var json_txt = File.ReadAllText(PATH_CONFIG_DOORS);
+                Doors = JsonConvert.DeserializeObject<Doors>(json_txt);
+            }
+            catch { }
+        }
+
+    }
+
+
+
+    [Serializable]
+    class DoorID { public int opened, closed; }
+    [Serializable]
+    class DoorsCategory { public String name; public List<DoorID> door; }
+    [Serializable]
+    class Doors { public List<DoorsCategory> doors;  }
+
+}
