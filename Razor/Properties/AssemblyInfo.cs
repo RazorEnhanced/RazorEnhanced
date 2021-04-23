2e4eabf458e15c8ac64e1694c1d75ddf799bceb0
Credzba <credzba@gmail.com>
2021-04-22 21:28:25 -0500

    version 7.7.18

--- Razor/Properties/AssemblyInfo.cs
+++ Razor/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.7.7.16")]
+[assembly: AssemblyVersion("0.7.7.18")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
