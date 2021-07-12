23f5423759beae1ca02957a44dc656b17d06e54f
Simone Caporale <caporalesimone@mirage>
2021-07-12 21:18:38 +0200

    Ultima.dll included by default before build C# scripts

--- Razor/RazorEnhanced/CSharpEngine.cs
+++ Razor/RazorEnhanced/CSharpEngine.cs
@@ -68,6 +68,8 @@ namespace RazorEnhanced
             }
 
             list.Add(Assistant.Engine.RootPath + "\\" + "RazorEnhanced.exe");
+            list.Add(Assistant.Engine.RootPath + "\\" + "Ultima.dll");
+
             return list;
         }
 
