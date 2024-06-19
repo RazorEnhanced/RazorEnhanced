43cf19266a97e7bb9c3b671a0cabb4dbabecf220
credzba <credzba@gmail.com>
2024-06-19 13:56:06 +0800

    Partial fix for gumps

--- Razor/Properties/AssemblyInfo.cs
+++ Razor/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.8.2.141")]
+[assembly: AssemblyVersion("0.8.2.142")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
