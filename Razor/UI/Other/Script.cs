6cba9a7cda8149bdbc1c42b8ca3fc9e6d55c611e
credzba <credzba@gmail.com>
2024-08-26 17:13:44 +0800

    fix remove script issue

--- Razor/UI/Other/Script.cs
+++ Razor/UI/Other/Script.cs
@@ -600,7 +600,7 @@ namespace Assistant
                 for (int i=0; i< list.Count; i++)
                 {
                     Scripts.ScriptItem item = list[i];
-                    if (item.Filename == scriptListView.SelectedItems[0].Text)
+                    if (item.Filename.ToLower() == scriptListView.SelectedItems[0].Text.ToLower())
                     {
                         removeIndex = i;
                         break;
